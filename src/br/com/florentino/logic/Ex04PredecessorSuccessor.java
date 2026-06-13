7979b33d3ccc5b9338b517a8c4c12db7ff1e457e
Florentino Cazé <florentinocasn@gmail.com>
2026-06-13 16:22:12 -0300

    feat: add Ex04PredecessorSuccessor

---
+++ src/br/com/florentino/logic/Ex04PredecessorSuccessor.java
@@ -0,0 +1,21 @@
+package br.com.florentino.logic;
+
+import java.util.Scanner;
+
+/**
+ * Leia um inteiro. Imprima seu antecessor e seu sucessor.
+ */
+
+public class Ex04PredecessorSuccessor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Insira um número inteiro: ");
+        int A = scanner.nextInt();
+
+        int pred = A - 1;
+        int succ = A + 1;
+
+        System.out.println("O antecessor de " + A + " é " + pred + ", enquanto o sucessor é " + succ + ".");
+    }
+}
