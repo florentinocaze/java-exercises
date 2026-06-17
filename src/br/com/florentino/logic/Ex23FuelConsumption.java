4e389061fe2c1cb28ea10faa80a56629af8cdeef
Florentino Cazé <florentinocasn@gmail.com>
2026-06-17 14:51:56 -0300

    feat: add Ex23FuelConsumption

---
+++ src/br/com/florentino/logic/Ex23FuelConsumption.java
@@ -0,0 +1,30 @@
+package br.com.florentino.logic;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+/**
+ * Calcule litros gastos em viagem dado tempo e velocidade média.
+ */
+
+public class Ex23FuelConsumption {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("0.00");
+
+        System.out.print("Informe o tempo da viagem em horas: ");
+        int travelTime = scanner.nextInt();
+
+        System.out.print("Informe a velocidade média em km/h: ");
+        int averageSpeed = scanner.nextInt();
+
+        int distanceTraveled = travelTime * averageSpeed;
+        int kmPerLiter = 12;
+
+        double fuelConsumption = (double) distanceTraveled / kmPerLiter;
+
+        System.out.println("A distância percorrida na viagem é de " + distanceTraveled + " km, acarretando o consumo de " + df.format(fuelConsumption) + " litros.");
+
+        scanner.close();
+    }
+}
