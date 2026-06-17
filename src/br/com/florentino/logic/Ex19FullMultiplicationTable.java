496612b4582dc05820859175d8455ba86abd46c4
Florentino Cazé <florentinocasn@gmail.com>
2026-06-17 14:18:05 -0300

    feat: add Ex19FullMultiplicationTable

---
+++ src/br/com/florentino/logic/Ex19FullMultiplicationTable.java
@@ -0,0 +1,17 @@
+package br.com.florentino.logic;
+
+/**
+ * Imprima a tabuada de 1 a 10.
+ */
+
+public class Ex19FullMultiplicationTable {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Tabuada do " + i);
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(i + " x " + j + " = " + (i * j));
+            }
+            System.out.println();
+        }
+    }
+}
