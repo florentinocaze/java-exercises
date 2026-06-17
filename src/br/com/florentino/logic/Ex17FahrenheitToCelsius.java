956ba186c8852f06bd4d7f5d547aacf4a73874d6
Florentino Cazé <florentinocasn@gmail.com>
2026-06-17 13:22:47 -0300

    feat: add Ex17FahrenheitToCelsius

---
+++ src/br/com/florentino/logic/Ex17FahrenheitToCelsius.java
@@ -0,0 +1,24 @@
+package br.com.florentino.logic;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+/**
+ * Leia temperatura em Fahrenheit e converta para Celsius.
+ */
+
+public class Ex17FahrenheitToCelsius {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("0.0");
+
+        System.out.print("Insira o valor da temperatura em Fahrenheit (º F): ");
+        double temperature = scanner.nextDouble();
+
+        double fahrenheitToCelsius = (temperature - 32) / 1.8;
+
+        System.out.println(temperature + "º F equivale a " + df.format(fahrenheitToCelsius) + "º C.");
+
+        scanner.close();
+    }
+}
